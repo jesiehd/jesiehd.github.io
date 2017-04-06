274659d674ce9339e8ac65f605902455adba9631
jesiehd <jesiehd@gmail.com>
2017-04-06 18:06:18 +1000

    Initial website uploaded

---
+++ js/custom.js
@@ -0,0 +1,56 @@
+
+
+$('.main-caro').slick({
+  dots: false,
+  infinite: true,
+  autoplay: false,
+  autoplaySpeed: 1000,
+  slidesToShow: 1,
+  adaptiveHeight: true,
+  mobileFirst: true,
+  // responsive: [
+  //     {
+  //       breakpoint: 992,
+  //       settings: {
+  //         slidesToShow: 1,
+  //         slidesToScroll: 1,
+  //         infinite: true,
+  //         dots: true
+  //       }
+  //     },
+  //     {
+  //       breakpoint: 768,
+  //       settings: {
+  //         slidesToShow: 1,
+  //         slidesToScroll: 1
+  //       }
+  //     },
+  //     {
+  //       breakpoint: 480,
+  //       settings: {
+  //         slidesToShow: 1,
+  //         slidesToScroll: 1
+  //       }
+  //     }
+  //   // You can unslick at a given breakpoint now by adding:
+  //   // settings: "unslick"
+  //   // instead of a settings object
+  //   ]
+});
+
+
+var $grid = $('.grid').isotope({
+  // options
+  itemSelector: '.element-item',
+  masonry: {
+    isFitWidth: true,
+  }
+});
+
+  // bind filter button click
+$('.filter-button-group').on( 'click', 'button', function() {
+  var filterValue = $( this ).attr('data-filter');
+  $grid.isotope({ filter: filterValue });
+});
+
+
